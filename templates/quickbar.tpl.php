fa1ae0eeef0c62afdc5dd8d6afa4f1d138ff0f5c
Cyle Carlson <jacerider@gmail.com>
2014-01-17 15:01:08 -0600

    Add quickbar support and more fawesome icons.

---
+++ templates/quickbar.tpl.php
@@ -0,0 +1,14 @@
+<nav id="quickbar-top-bar" class="top-bar" data-topbar="" data-options="is_hover: false">
+    <ul class="title-area">
+        <li class="name">
+            <h1><a href="/admin">Admin</a></h1>
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+    </ul>
+
+    <section class="top-bar-section">
+      <?php foreach ($tree_0 as $links): ?>
+        <?php print $links; ?>
+      <?php endforeach; ?>
+    </section>
+</nav>
