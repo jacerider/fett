8c44ab9d1b7d929eb8b81def9d1420ff5593edac
Cyle Carlson <jacerider@gmail.com>
2016-09-07 10:36:04 -0500

    Use strict and support for eslint.

--- dev/js/fett.fastclick.js
+++ dev/js/fett.fastclick.js
@@ -2,10 +2,16 @@
  * @file
  * Initialize fastclick.
  */
+
+/* global FastClick */
 (function ($, document) {
+
+  'use strict';
+
   Drupal.behaviors.FettFastclick = {
     attach: function (context) {
       FastClick.attach(document.body);
     }
   };
+
 }(jQuery, document));
