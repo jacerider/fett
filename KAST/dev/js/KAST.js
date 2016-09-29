6fff94d0bd6d1ddbcb028200420f851e50ce5cf6
Cyle Carlson <jacerider@gmail.com>
2016-09-29 15:03:34 -0500

    Add placeholder for js.

---
+++ KAST/dev/js/KAST.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * KAST Core.
+ */
+(function ($, window, document) {
+
+  'use strict';
+
+  Drupal.behaviors.KAST = {
+    attach: function (context, settings) {
+    }
+  };
+
+}(jQuery, window, document));
